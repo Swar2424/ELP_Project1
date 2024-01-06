c5cfccbd1d1b5dca39a2169d58b73a4110b0a31e
Félix Alonzo <felix.alonzo14@gmail.com>
2024-01-06 16:28:49 +0100

    true start of project : read int from file

--- go/package main.go
+++ go/package main.go
@@ -2,11 +2,31 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strconv"
 )
 
-var c, python, java bool
-
 func main() {
-	i := 4
-	fmt.Println(i)
+	b := make([]byte, 1)
+	N := 4
+	var dat_numb [4]int
+	var dat_str, dat_str_list string
+
+	dat, _ := os.Open("./textfile_test.txt")
+
+	for i := 0; i < N; {
+		dat_read, _ := dat.Read(b)
+		dat_str = string(b[:dat_read])
+
+		if dat_str != " " {
+			dat_str_list += dat_str
+		} else {
+			dat_int, _ := strconv.Atoi(dat_str_list)
+			dat_numb[i] = dat_int
+			i++
+			dat_str_list = ""
+		}
+	}
+
+	fmt.Println(dat_numb)
 }
