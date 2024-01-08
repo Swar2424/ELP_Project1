55e2b7eef9576da4f8cc39cdbae9b966ed30e94f
Félix Alonzo <felix.alonzo14@gmail.com>
2024-01-08 16:25:09 +0100

    dijkstra goroutines

--- go/Main.go
+++ go/Main.go
@@ -2,17 +2,22 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
+	time_start := time.Now()
 	Canal := make(chan ([]int))
-	dat_numb := Load_data("./textfile_test.txt", 4)
-	fmt.Println(dat_numb)
+	n := 500
+	dat_numb := Load_data("./data", n)
 
-	for i := 0; i < 4; i++ {
+	for i := 0; i < n; i++ {
 		go Dijkstra(dat_numb, i, Canal)
 	}
-	for i := 0; i < 4; i++ {
+	for i := 0; i < n; i++ {
 		fmt.Println(<-Canal)
 	}
+
+	time_end := time.Now()
+	fmt.Println(time_end.Sub(time_start))
 }
