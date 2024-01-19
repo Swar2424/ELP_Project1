e85872f61c7a03fafb37a3def5d54be72c447b06
Swar2424 <154347168+Swar2424@users.noreply.github.com>
2024-01-19 14:53:11 +0100

    ça marche pas et je comprends pas

--- elm/src/Main_class.elm
+++ elm/src/Main_class.elm
@@ -1,12 +1,23 @@
 module Main_class exposing (..)
 
 
+-- Press a button to send a GET request for random quotes.
+--
+-- Read how it works:
+--   https://guide.elm-lang.org/effects/json.html
+--
+
 import Browser
 import Html exposing (..)
 import Html.Attributes exposing (style)
 import Html.Events exposing (..)
 import Http
 import Json.Decode exposing (Decoder, map4, field, int, string)
+
+
+
+-- MAIN
+
 
 main =
   Browser.element
